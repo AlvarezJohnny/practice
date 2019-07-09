36b84f8b6c12b43a0386c04351d5167e30bf016a
Jonathan Alvarez <alvarezjohnny3@gmail.com>
2019-07-09 14:38:10 -0400

    heroku

--- server.rb
+++ server.rb
@@ -11,6 +11,40 @@ end
 require "active_record"
 ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
 
+class User
+  attr_accessor :email, :password, :id
+  def initialize(email, password)
+    @email = email
+    @password = password
+  end
+
+  def valid?
+    if (@email != '' && @password != '' && @password.length > 7)
+      return true
+    end
+  end
+
+  def save
+    $db.execute("INSERT INTO users (email, password)
+    VALUES ('#{@email}', '#{@password}');")
+    return true
+  end
+
+  def self.all
+    @all = $db.execute("SELECT * FROM users;")
+    return @all
+  end
+
+   def self.find(id)
+     @user = $db.execute("SELECT * FROM users WHERE id = '#{id}'")
+     return @user
+   end
+
+  def delete?(id)
+    @user = $db.execute("DELETE FROM users WHERE id = '#{id}'")
+    return @user
+end
+
 get "/" do
   erb :home
 end
@@ -19,26 +53,27 @@ get "/signup" do
   erb :signup
 end
 
+get "/users" do
+  @users = User.all
+  erb :users
+end
+
+get '/user/:id' do
+
+end
+
 post "/signup" do
-  user = User.new(username: params[:username], password: params[:password])
-  p user
-  user.save
-  redirect "/"
+  p "POST request recieved"
+  p params
+  @user = User.new(params[@email], params[@password])
+  if @user.valid?
+    @user.save
+    redirect "/thank-you", 301
+  else
+    puts 'This user is missing information'
+  end
 end
 
-get "/login" do
-  erb :login
-end
-
-post "/login" do
-  username = params[:username]
-  given_password = params[:password]
-  user = User.find_by(username: username)
-  session[:user] = user
-  redirect "/"
-end
-
-
-post "/logout" do
-
+get "/thank-you" do
+  erb :thanks
 end
