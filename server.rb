fe2fd860490d65dc7d1409b3472ca6971249fef1
Jonathan Alvarez <alvarezjohnny3@gmail.com>
2019-07-09 13:14:54 -0400

    heroku

--- server.rb
+++ server.rb
@@ -5,7 +5,8 @@ enable :sessions
 class User < ActiveRecord::Base
 end
 
-ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "./db.sqlite3")
+# ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "./db.sqlite3")
+ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
 
 get "/" do
   erb :home
